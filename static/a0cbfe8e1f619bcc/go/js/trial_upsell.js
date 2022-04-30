4ec88363256e57ea940a626739fb27c62a573763
davidb20-0 <david.binng@gmail.com>
2022-04-30 22:42:38 +0100

    yooooo

---
+++ static/a0cbfe8e1f619bcc/go/js/trial_upsell.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(a){a(document).on("click",'[data-action="upsell-freetrial"]',function(f){var d=a(this),c=a(this).data("hook"),b=false;f.preventDefault();switch(c){case"unlock":b="#trial-unlock-overlay";break;case"expired":b="#trial-expired-overlay";break;case"biz-content":b="#trial-biz-content-overlay";break}if(b&&a(b).length>0){a(b).modal()}})})(jQuery);
+
+}
+/*
+     FILE ARCHIVED ON 15:42:37 Dec 04, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:08:38 Apr 26, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 97.568
+  exclusion.robots: 0.165
+  exclusion.robots.policy: 0.158
+  RedisCDXSource: 14.199
+  esindex: 0.009
+  LoadShardBlock: 63.127 (3)
+  PetaboxLoader3.datanode: 98.647 (5)
+  CDXLines.iter: 18.012 (3)
+  load_resource: 1085.366 (2)
+  PetaboxLoader3.resolve: 1023.186 (2)
+*/
